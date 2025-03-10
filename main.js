0c39f557fe75defaa198a4a91de3e01046909f8a
yfussy <toainth@gmail.com>
2025-03-10 21:58:35 +0700

    edit 2

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 console.log("Hello from main!");
-console.log("!Resolved conflict from sub_branch_1!")
+console.log("!Resolved conflict from sub_branch_1!");
+console.log("Edit 2 from sub_branch_1")
